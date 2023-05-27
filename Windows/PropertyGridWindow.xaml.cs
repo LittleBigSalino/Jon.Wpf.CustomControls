989713bce06d076bd076fb2bb0753060f90a68e5
Jon M. Sales <RealSalino@outlook.com>
2023-05-26 21:49:58 -0700

    PropertyGridWindow added.

---
+++ Windows/PropertyGridWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace Jon.Wpf.CustomControls.Windows
+{
+    /// <summary>
+    /// Interaction logic for PropertyGridWindow.xaml
+    /// </summary>
+    public partial class PropertyGridWindow : Window
+    {
+        public object SelectedObject
+        {
+            get { return GetValue(SelectedObjectProperty); }
+            set { SetValue(SelectedObjectProperty, value); }
+        }
+
+        public static readonly DependencyProperty SelectedObjectProperty =
+           DependencyProperty.Register("SelectedObject", typeof(object), typeof(PropertyGridWindow), new PropertyMetadata(null));
+
+
+        public PropertyGridWindow()
+        {
+            InitializeComponent();
+        }
+
+        public PropertyGridWindow(object parameter)
+        {
+            InitializeComponent();
+            SelectedObject = parameter;
+            PropertyGridMain.SelectedObject = SelectedObject;
+            Title = $"Object of type : {parameter.GetType().ToString()}";
+        }
+    }
+}
